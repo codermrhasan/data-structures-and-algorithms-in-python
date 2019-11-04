420a055d34cdb78206789383a0279f63cc898113
Md Rakibul Hasan <rahacode@gmail.com>
2019-11-04 22:59:32 +0600

    checking push method to push data in linked list

--- src/linked_list/tests/test_singly_linked_list.py
+++ src/linked_list/tests/test_singly_linked_list.py
@@ -2,4 +2,26 @@ from linked_list.singly_linked_list import SinglyLinkedList
 
 
 def test_exists():
+    """
+    checking existance of SinglyLinkedList class
+    """
     assert SinglyLinkedList
+
+
+def test_instantiation():
+    """
+    checking if it is successfully instantiate an empty linked list?
+    """
+    assert SinglyLinkedList()
+
+
+def test_push():
+    """
+    Can we properly push a data to linked list or not? new data must be in head
+    """
+    names = SinglyLinkedList()
+    names.push('John')
+    expected = 'John'
+    actual = names.head.value
+
+    assert actual == expected
